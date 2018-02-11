fc7fa74e287b087cc0f919d62b5be0a05f3a0eed
wcdaren@gmail.com <apple@wangchen.local>
2018-02-11 14:45:47 +0800

    node部署妄称

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var http = require('http'),
 var server = http.createServer(function (req, res) {
     var urlObj = url.parse(req.url, true),
         pathname = urlObj.pathname,
-        query = urlObj.query;
+        query = urlObj.query;//存储的是客户端请求的url地址中问号传参后面的信息
 
     var reg = /\.(HTML|JS|CSS|JSON|TXT|ICO)/i;
 
@@ -43,14 +43,18 @@ var server = http.createServer(function (req, res) {
         return
     }
     //->api数据结构的处理
-    var con = null, result = null, customPath = "./json/custom.json";
+    var con = null,
+        result = null,
+        customId = null,
+        customPath = "./json/custom.json";
+
     //->API数据接口的处理
+    con = fs.readFileSync(customPath, "utf-8")
+    con.length === 0 ? con = '[]' : null //防止custom.json无数据
+    con = JSON.parse(con)
+
     //1.获取所有的客户信息   
     if (pathname === "/getList") {
-        con = fs.readFileSync(customPath, "utf-8")
-        con.length === 0 ? con = '[]' : null //防止custom.json无数据
-        con = JSON.parse(con)
-
         result = {
             code: 1,
             msg: "没有任何的客户信息",
@@ -63,12 +67,141 @@ var server = http.createServer(function (req, res) {
                 data: con,
             }
         }
-        res.writeHead(200,{'content-type':'application/json;charset=utf-8;'})
+        res.writeHead(200, { 'content-type': 'application/json;charset=utf-8;' })
         res.end(JSON.stringify(result))
-        return 
+        return
 
     }
-});
+
+    //2.获取具体的某一个客户的信息
+    if (pathname === "/getInfo") {
+        customId = query["id"]
+
+        result = {
+            code: 1,
+            msg: "客户不存在",
+            data: null,
+        }
+        for (var i = 0; i < con.length; i++) {
+            if (con[i]['id'] === customId) {
+                result = {
+                    code: 0,
+                    msg: "成功",
+                    data: con[i],
+                }
+                break
+            }
+        }
+        res.writeHead(200, { 'content-type': 'application/json;charset=utf-8;' })
+        res.end(JSON.stringify(result))
+        return
+
+    }
+
+    //3.删除客户信息
+    if (pathname === "/removeInfo") {
+        customId = query["id"]
+        var flag = false
+
+        for (var i = 0; i < con.length; i++) {
+            if (con[i]["id"] === customId) {
+                con.splice(i, 1)
+                flag = true
+                break
+            }
+        }
+        result = {
+            code: 1,
+            msg: "删除失败",
+        }
+
+        if (flag) {
+            fs.writeFileSync(customPath, JSON.stringify(con), "utf-8")
+            result = {
+                code: 0,
+                msg: "删除成功",
+            }
+
+        }
+        res.writeHead(200, { 'content-type': 'application/json;charset=utf-8;' })
+        res.end(JSON.stringify(result))
+        return
+    }
+
+    //4.增加客户信息
+    if (pathname === "/getInfo") {
+        var str = ''
+
+        req.on("data", function (chunk) {
+            str += chunk
+        })
+        req.on("end", function () {
+            if (str.length === 0) {
+                res.writeHead(200, { 'content-type': 'application/json;charset=utf-8;' })
+                res.end(JSON.stringify({
+                    code: 1,
+                    msg: "增加失败,没有传递任何需要增加的信息",
+                }))
+                return
+            }
+            var data = JSON.parse(str)
+
+            data["id"] = con.length === 0 ? 1 : parseFloat(con[con.length - 1]["id"]) + 1
+            con.push(data)
+            fs.writeFileSync(customPath, JSON.stringify(con), "utf-8")
+            res.end(JSON.stringify({
+                code: 0,
+                msg: "增加成功",
+            }))
+        })
+        return
+    }
+
+    //5.修改客户信息
+    if (pathname === "/updateInfo") {
+        var str = ''
+        req.on("data", function (chunk) {
+            str += chunk
+        })
+        req.on("end", function () {
+            if (str.length === 0) {
+                res.writeHead(200, { 'content-type': 'application/json;charset=utf-8;' })
+                res.end(JSON.stringify({
+                    code: 1,
+                    msg: "修改失败,没有传递任何需要修改的信息",
+                }))
+                return
+            }
+
+            var flag = false,
+                data = JSON.parse(str)
+            for (var i = 0; i < con.length; i++) {
+                if (con[i]["id"] === data["id"]) {
+                    con[i] = data
+                    flag = true
+                    break
+                }
+            }
+            result.msg = "修改失败，需要修改的客户不存在"
+            if (flag) {
+                fs.writeFileSync(customPath, JSON.stringify(con), "utf-8")
+                result = {
+                    code: 0,
+                    msg: "修改成功"
+                }
+            }
+
+            res.writeHead(200, { 'content-type': 'application/json;charset=utf-8;' })
+            res.end(JSON.stringify(result))
+        })
+
+        return
+    }
+
+    res.writeHead(404, { 'content-type': 'text/plain;charset=utf-8;' })
+    res.end("请求的数据接口不存在", )
+
+})
 
 //为这个服务器监听一个端口
 server.listen(8080, function () {
